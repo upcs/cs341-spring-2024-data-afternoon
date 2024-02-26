acc924552b72a557ae229da7d9b71a9cc0a63fec
LKenny26 <kenny26@up.edu>
2024-02-25 16:58:45 -0800

    key added when map is clicked

--- data-afternoon/public/javascripts/unhoused.js
+++ data-afternoon/public/javascripts/unhoused.js
@@ -1,6 +1,20 @@
 /* java scripts for unhoused project */
 
-  function mydataFunction() {
-    alert("This location is available now! ")
-   }
+function mydataFunction() {
+  alert("This location is available now! ")
+}
+
+function addInfoMap() {
+  //get rid of dropdown and add in key
+  document.getElementById("DropDown").style.display = 'none';
+  document.getElementById("key").style.display = 'block';
+}
+
+function changeAddInfoBack() {
+  //temporary since we do not have things to put 
+  //in additional info for other tabs
+  document.getElementById("DropDown").style.display = 'block';
+  document.getElementById("key").style.display = 'add';
+}
+
 module.exports = { mydataFunction };
