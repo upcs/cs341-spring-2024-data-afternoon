b6580b4828b6d1a6764395518f9b46edcd723ad1
leond25 <leond25@up.edu>
2024-03-15 15:05:36 -0700

    map pins get data from database in services page

--- data-afternoon/app.js
+++ data-afternoon/app.js
@@ -13,6 +13,7 @@ const promise2 = exports.dbquery("SELECT * FROM FREEGROCERIES");
 const promise3 = exports.dbquery("SELECT * FROM SAFERESTVILLAGES");
 const promise4 = exports.dbquery("SELECT * FROM FREE_PRODUCE");
 const promise5 = exports.dbquery("SELECT * FROM SAFE_YEAR_ROUND_SHELTERS");
+// "'p:YK<>1p\B{t8;X   (database passcode)
 
 var app = express();
 
@@ -20,6 +21,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//this post gets all the data from the database
 app.post('/', (req, res) => {
 
  promise.then(result => {
@@ -27,158 +29,44 @@ app.post('/', (req, res) => {
     promise3.then(result3 =>  {
       promise4.then(result4 =>  {
         promise5.then(result5 =>  {
-  
-  let FREEMEALS = [ //get freemeals data
-    [JSON.stringify(result[0].LOCATION), JSON.stringify(result[0].NAME)], 
-    [JSON.stringify(result[1].LOCATION), JSON.stringify(result[1].NAME)],
-    [JSON.stringify(result[2].LOCATION), JSON.stringify(result[2].NAME)],
-    [JSON.stringify(result[3].LOCATION), JSON.stringify(result[3].NAME)],
-    [JSON.stringify(result[4].LOCATION), JSON.stringify(result[4].NAME)],
-    [JSON.stringify(result[5].LOCATION), JSON.stringify(result[5].NAME)],
-    [JSON.stringify(result[6].LOCATION), JSON.stringify(result[6].NAME)],
-    [JSON.stringify(result[7].LOCATION), JSON.stringify(result[7].NAME)],
-    [JSON.stringify(result[8].LOCATION), JSON.stringify(result[8].NAME)],
-    [JSON.stringify(result[9].LOCATION), JSON.stringify(result[9].NAME)]
-    ];
 
-    
-    let FREEGROCERIES = [ //get freegroceries data
-      [JSON.stringify(result2[0].LOCATION), JSON.stringify(result2[0].NAME)], 
-      [JSON.stringify(result2[1].LOCATION), JSON.stringify(result2[1].NAME)],
-      [JSON.stringify(result2[2].LOCATION), JSON.stringify(result2[2].NAME)],
-      [JSON.stringify(result2[3].LOCATION), JSON.stringify(result2[3].NAME)], 
-      [JSON.stringify(result2[4].LOCATION), JSON.stringify(result2[4].NAME)],
-      [JSON.stringify(result2[5].LOCATION), JSON.stringify(result2[5].NAME)],
-      [JSON.stringify(result2[6].LOCATION), JSON.stringify(result2[6].NAME)], 
-      [JSON.stringify(result2[7].LOCATION), JSON.stringify(result2[7].NAME)],
-      [JSON.stringify(result2[8].LOCATION), JSON.stringify(result2[8].NAME)],
-      [JSON.stringify(result2[9].LOCATION), JSON.stringify(result2[9].NAME)], 
-      [JSON.stringify(result2[10].LOCATION), JSON.stringify(result2[10].NAME)],
-      [JSON.stringify(result2[11].LOCATION), JSON.stringify(result2[11].NAME)],
-      [JSON.stringify(result2[12].LOCATION), JSON.stringify(result2[12].NAME)], 
-      [JSON.stringify(result2[13].LOCATION), JSON.stringify(result2[13].NAME)],
-      [JSON.stringify(result2[14].LOCATION), JSON.stringify(result2[14].NAME)],
-      [JSON.stringify(result2[15].LOCATION), JSON.stringify(result2[15].NAME)], 
-      [JSON.stringify(result2[16].LOCATION), JSON.stringify(result2[16].NAME)],
-      [JSON.stringify(result2[17].LOCATION), JSON.stringify(result2[17].NAME)],
-      [JSON.stringify(result2[18].LOCATION), JSON.stringify(result2[18].NAME)], 
-      [JSON.stringify(result2[19].LOCATION), JSON.stringify(result2[19].NAME)],
-      [JSON.stringify(result2[20].LOCATION), JSON.stringify(result2[20].NAME)],
-      [JSON.stringify(result2[21].LOCATION), JSON.stringify(result2[21].NAME)], 
-      [JSON.stringify(result2[22].LOCATION), JSON.stringify(result2[22].NAME)],
-      [JSON.stringify(result2[23].LOCATION), JSON.stringify(result2[23].NAME)],
-      [JSON.stringify(result2[24].LOCATION), JSON.stringify(result2[24].NAME)], 
-      [JSON.stringify(result2[25].LOCATION), JSON.stringify(result2[25].NAME)],
-      [JSON.stringify(result2[26].LOCATION), JSON.stringify(result2[26].NAME)],
-      [JSON.stringify(result2[27].LOCATION), JSON.stringify(result2[27].NAME)], 
-      [JSON.stringify(result2[28].LOCATION), JSON.stringify(result2[28].NAME)],
-      [JSON.stringify(result2[29].LOCATION), JSON.stringify(result2[29].NAME)],
-      [JSON.stringify(result2[30].LOCATION), JSON.stringify(result2[30].NAME)], 
-      [JSON.stringify(result2[31].LOCATION), JSON.stringify(result2[31].NAME)],
-      [JSON.stringify(result2[32].LOCATION), JSON.stringify(result2[32].NAME)],
-      [JSON.stringify(result2[33].LOCATION), JSON.stringify(result2[33].NAME)], 
-      [JSON.stringify(result2[34].LOCATION), JSON.stringify(result2[34].NAME)],
-      [JSON.stringify(result2[35].LOCATION), JSON.stringify(result2[35].NAME)],
-      [JSON.stringify(result2[36].LOCATION), JSON.stringify(result2[36].NAME)],
-      [JSON.stringify(result2[37].LOCATION), JSON.stringify(result2[37].NAME)],
-      [JSON.stringify(result2[38].LOCATION), JSON.stringify(result2[38].NAME)], 
-      [JSON.stringify(result2[39].LOCATION), JSON.stringify(result2[39].NAME)],
-      [JSON.stringify(result2[40].LOCATION), JSON.stringify(result2[40].NAME)],
-      [JSON.stringify(result2[41].LOCATION), JSON.stringify(result2[41].NAME)], 
-      [JSON.stringify(result2[42].LOCATION), JSON.stringify(result2[42].NAME)],
-      [JSON.stringify(result2[43].LOCATION), JSON.stringify(result2[43].NAME)],
-      [JSON.stringify(result2[44].LOCATION), JSON.stringify(result2[44].NAME)], 
-      [JSON.stringify(result2[45].LOCATION), JSON.stringify(result2[45].NAME)],
-      [JSON.stringify(result2[46].LOCATION), JSON.stringify(result2[46].NAME)],
-      [JSON.stringify(result2[47].LOCATION), JSON.stringify(result2[47].NAME)], 
-      [JSON.stringify(result2[48].LOCATION), JSON.stringify(result2[48].NAME)],
-      [JSON.stringify(result2[49].LOCATION), JSON.stringify(result2[49].NAME)],
-      [JSON.stringify(result2[50].LOCATION), JSON.stringify(result2[50].NAME)], 
-      [JSON.stringify(result2[51].LOCATION), JSON.stringify(result2[51].NAME)],
-      [JSON.stringify(result2[52].LOCATION), JSON.stringify(result2[52].NAME)],
-      [JSON.stringify(result2[53].LOCATION), JSON.stringify(result2[53].NAME)], 
-      [JSON.stringify(result2[54].LOCATION), JSON.stringify(result2[54].NAME)],
-      [JSON.stringify(result2[55].LOCATION), JSON.stringify(result2[55].NAME)],
-      [JSON.stringify(result2[56].LOCATION), JSON.stringify(result2[56].NAME)], 
-      [JSON.stringify(result2[57].LOCATION), JSON.stringify(result2[57].NAME)],
-      [JSON.stringify(result2[58].LOCATION), JSON.stringify(result2[58].NAME)],
-      [JSON.stringify(result2[59].LOCATION), JSON.stringify(result2[59].NAME)],
-      [JSON.stringify(result2[60].LOCATION), JSON.stringify(result2[60].NAME)], 
-      [JSON.stringify(result2[61].LOCATION), JSON.stringify(result2[61].NAME)],
-      [JSON.stringify(result2[62].LOCATION), JSON.stringify(result2[62].NAME)],
-      [JSON.stringify(result2[63].LOCATION), JSON.stringify(result2[63].NAME)],
-      [JSON.stringify(result2[64].LOCATION), JSON.stringify(result2[64].NAME)]
-      ];
-      
-      let SAFERESTVILLAGES = [ //get safe rest villages data
-      [JSON.stringify(result3[0].SITENAME), JSON.stringify(result3[0].TOTALSLEEPINGUNITS), JSON.stringify(result3[0].LOCATION)], 
-      [JSON.stringify(result3[1].SITENAME), JSON.stringify(result3[1].TOTALSLEEPINGUNITS), JSON.stringify(result3[1].LOCATION)],
-      [JSON.stringify(result3[2].SITENAME), JSON.stringify(result3[2].TOTALSLEEPINGUNITS), JSON.stringify(result3[2].LOCATION)],
-      [JSON.stringify(result3[3].SITENAME), JSON.stringify(result3[3].TOTALSLEEPINGUNITS), JSON.stringify(result3[3].LOCATION)], 
-      [JSON.stringify(result3[4].SITENAME), JSON.stringify(result3[4].TOTALSLEEPINGUNITS), JSON.stringify(result3[4].LOCATION)],
-      [JSON.stringify(result3[5].SITENAME), JSON.stringify(result3[5].TOTALSLEEPINGUNITS), JSON.stringify(result3[5].LOCATION)],
-      [JSON.stringify(result3[6].SITENAME), JSON.stringify(result3[6].TOTALSLEEPINGUNITS), JSON.stringify(result3[6].LOCATION)]
-      ]
+          //get free meals data
+          let freeMeals = result.map(freeMealsTable => ({
+            location: freeMealsTable.LOCATION,
+            name: freeMealsTable.NAME,
+          }));
+          
+          //get free groceries data
+          let freeGroceries = result2.map(freeGroceriesTable => ({
+            location: freeGroceriesTable.LOCATION,
+            name: freeGroceriesTable.NAME,
+          }));
 
-      let FREE_PRODUCE = [ //get free produce data
-        [JSON.stringify(result4[0].NAME), JSON.stringify(result4[0].LOCATION), JSON.stringify(result4[0].HOURS_OPEN)], 
-        [JSON.stringify(result4[1].NAME), JSON.stringify(result4[1].LOCATION), JSON.stringify(result4[1].HOURS_OPEN)],
-        [JSON.stringify(result4[2].NAME), JSON.stringify(result4[2].LOCATION), JSON.stringify(result4[2].HOURS_OPEN)],
-        [JSON.stringify(result4[3].NAME), JSON.stringify(result4[3].LOCATION), JSON.stringify(result4[3].HOURS_OPEN)], 
-        [JSON.stringify(result4[4].NAME), JSON.stringify(result4[4].LOCATION), JSON.stringify(result4[4].HOURS_OPEN)],
-        [JSON.stringify(result4[5].NAME), JSON.stringify(result4[5].LOCATION), JSON.stringify(result4[5].HOURS_OPEN)],
-        [JSON.stringify(result4[6].NAME), JSON.stringify(result4[6].LOCATION), JSON.stringify(result4[6].HOURS_OPEN)], 
-        [JSON.stringify(result4[7].NAME), JSON.stringify(result4[7].LOCATION), JSON.stringify(result4[7].HOURS_OPEN)],
-        [JSON.stringify(result4[8].NAME), JSON.stringify(result4[8].LOCATION), JSON.stringify(result4[8].HOURS_OPEN)],
-        [JSON.stringify(result4[9].NAME), JSON.stringify(result4[9].LOCATION), JSON.stringify(result4[9].HOURS_OPEN)], 
-        [JSON.stringify(result4[10].NAME), JSON.stringify(result4[10].LOCATION), JSON.stringify(result4[10].HOURS_OPEN)],
-        [JSON.stringify(result4[11].NAME), JSON.stringify(result4[11].LOCATION), JSON.stringify(result4[11].HOURS_OPEN)],
-        [JSON.stringify(result4[12].NAME), JSON.stringify(result4[12].LOCATION), JSON.stringify(result4[12].HOURS_OPEN)]
-        ]
+          //get safe rest villages data
+            let safeRestVillages = result3.map(safeRestTable => ({
+            location: safeRestTable.LOCATION,
+            name: safeRestTable.NAME
+          }));
+          
+          //get free produce data
+          let freeProduce = result4.map(freeProduceTable => ({
+            location: freeProduceTable.LOCATION,
+            name: freeProduceTable.NAME
+          }));
 
-        let SAFE_YEAR_ROUND_SHELTERS = [ //get safe year round shelters 
-          [JSON.stringify(result5[0].NAME), JSON.stringify(result5[0].LOCATION), JSON.stringify(result5[0].HOURS_OPEN)], 
-          [JSON.stringify(result5[1].NAME), JSON.stringify(result5[1].LOCATION), JSON.stringify(result5[1].HOURS_OPEN)],
-          [JSON.stringify(result5[2].NAME), JSON.stringify(result5[2].LOCATION), JSON.stringify(result5[2].HOURS_OPEN)],
-          [JSON.stringify(result5[3].NAME), JSON.stringify(result5[3].LOCATION), JSON.stringify(result5[3].HOURS_OPEN)], 
-          [JSON.stringify(result5[4].NAME), JSON.stringify(result5[4].LOCATION), JSON.stringify(result5[4].HOURS_OPEN)],
-          [JSON.stringify(result5[5].NAME), JSON.stringify(result5[5].LOCATION), JSON.stringify(result5[5].HOURS_OPEN)],
-          [JSON.stringify(result5[6].NAME), JSON.stringify(result5[6].LOCATION), JSON.stringify(result5[6].HOURS_OPEN)], 
-          [JSON.stringify(result5[7].NAME), JSON.stringify(result5[7].LOCATION), JSON.stringify(result5[7].HOURS_OPEN)],
-          [JSON.stringify(result5[8].NAME), JSON.stringify(result5[8].LOCATION), JSON.stringify(result5[8].HOURS_OPEN)],
-          [JSON.stringify(result5[9].NAME), JSON.stringify(result5[9].LOCATION), JSON.stringify(result5[9].HOURS_OPEN)], 
-          [JSON.stringify(result5[10].NAME), JSON.stringify(result5[10].LOCATION), JSON.stringify(result5[10].HOURS_OPEN)],
-          [JSON.stringify(result5[11].NAME), JSON.stringify(result5[11].LOCATION), JSON.stringify(result5[11].HOURS_OPEN)],
-          [JSON.stringify(result5[12].NAME), JSON.stringify(result5[12].LOCATION), JSON.stringify(result5[12].HOURS_OPEN)],
-          [JSON.stringify(result5[13].NAME), JSON.stringify(result5[13].LOCATION), JSON.stringify(result5[13].HOURS_OPEN)], 
-          [JSON.stringify(result5[14].NAME), JSON.stringify(result5[14].LOCATION), JSON.stringify(result5[14].HOURS_OPEN)],
-          [JSON.stringify(result5[15].NAME), JSON.stringify(result5[15].LOCATION), JSON.stringify(result5[15].HOURS_OPEN)],
-          [JSON.stringify(result5[16].NAME), JSON.stringify(result5[16].LOCATION), JSON.stringify(result5[16].HOURS_OPEN)]
-          ]
-
-          res.send(`
-          <h1>Community Services Information</h1>
-          <h2>Free Meals</h2>
-          <ul>
-            ${FREEMEALS.map(meal => `<li>Location: ${meal[0]}, Name: ${meal[1]}</li>`).join('')}
-          </ul>
-          <h2>Free Groceries</h2>
-          <ul>
-            ${FREEGROCERIES.map(grocery => `<li>Location: ${grocery[0]}, Name: ${grocery[1]}</li>`).join('')}
-          </ul>
-          <h2>Safe Rest Villages</h2>
-          <ul>
-            ${SAFERESTVILLAGES.map(village => `<li>Site Name: ${village[0]}, Total Sleeping Units: ${village[1]}, Location: ${village[2]}</li>`).join('')}
-          </ul>
-          <h2>Free Produce</h2>
-          <ul>
-            ${FREE_PRODUCE.map(produce => `<li>Name: ${produce[0]}, Location: ${produce[1]}, Hours Open: ${produce[2]}</li>`).join('')}
-          </ul>
-          <h2>Safe Year Round Shelters</h2>
-          <ul>
-            ${SAFE_YEAR_ROUND_SHELTERS.map(shelter => `<li>Name: ${shelter[0]}, Location: ${shelter[1]}, Hours Open: ${shelter[2]}</li>`).join('')}
-          </ul>
-        `);
+          //get safe year round shelters data
+          let safeYearRoundShelters = result5.map(safeYearTable => ({
+            location: safeYearTable.LOCATION,
+            name: safeYearTable.NAME
+          }));
+          
+          // Combining all tables into a single object
+          
+          let combinedResults = [ freeMeals, freeGroceries, safeRestVillages, freeProduce, safeYearRoundShelters] 
+          
+          
+          // Sending the combined results as a pretty-printed JSON string
+          res.send(combinedResults);
 
         });
       });
--- data-afternoon/public/javascripts/dataBase.js
+++ data-afternoon/public/javascripts/dataBase.js
@@ -1,8 +1,64 @@
 
+/* this method points a point in the map that corresponds to the sql address*/ 
+function geocodeAddress(geocoder, resultsMap, addressIn, nameIn) 
+{
+          
+        geocoder.geocode({ address: addressIn }, (results, status) => {
+                if (status === "OK") 
+                {
+
+                        resultsMap.setCenter(results[0].geometry.location); 
+
+                        new google.maps.Marker({
+
+                        map: resultsMap,
+                        position: results[0].geometry.location, //the point in the map which represents the address
+                        label: {
+                        fontSize: "8pt",
+                        text: nameIn
+                        }
+
+                        });
+                } 
+                else {
+                alert("Geocode was not successful for the following reason: " + status + " and here is address: " + addressIn);
+                }
+        });
+}
+
+
+/* send the client the map with pins that provide resources */
 $.post("http://localhost:3000/", function(data, status) {
 
-        document.getElementById("getDataBase").innerHTML = data; //database data 
+        var center = { lat: 45.5727, lng: 122.7215 }; //center the map around UP area
+      
+        // Create a new map object
+        var map = new google.maps.Map(document.getElementById("map"), {
+
+        zoom: 10, // Set the initial zoom level
+        center: center, // Set the center of the map
+
+        });
+
+        const geocoder = new google.maps.Geocoder(); //create a new geocoder object 
+
+
+        for (let i = 0; i < data.length; i++) //iterate through entire database 
+        {
+       
+                for (let j = 0; j < data[i].length; j++) //iterate through each table
+                {
+
+                     var address = JSON.stringify(data[i][j].location); //get current location from table 
+                     var name = JSON.stringify(data[i][j].name); //get current name from table
+
+                     geocodeAddress(geocoder, map, address, name); //insert the pin with location and name into the map
+
+                }
+        } 
+
 
 })
+
 
     
