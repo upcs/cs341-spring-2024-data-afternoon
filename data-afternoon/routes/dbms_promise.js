ded7f5168022d334613353f3919d02bbd9e5ad1d
tabra26 <tabra26@up.edu>
2024-04-30 20:52:42 -0700

    Changed Database connection

--- data-afternoon/routes/dbms_promise.js
+++ data-afternoon/routes/dbms_promise.js
@@ -19,17 +19,12 @@ var user = "root";         //username
 var password = "'p:YK<>1p\\B{t8;X";  //password 
 */
 
-/*
+
 var host = "35.247.8.163";    //from GCloud instance 
 var database = "UNHOUSED";  //database name
 var user = "root";         //username 
 var password = "'p:YK<>1p\\B{t8;X";  //password 
-*/
 
-var host = "localhost";    //from GCloud instance 
-var database = "UNHOUSED";  //database name
-var user = "root";         //username 
-var password = "password";  //password 
 
 
 //'p:YK<>1p\B{t8;X
